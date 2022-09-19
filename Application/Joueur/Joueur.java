68efa3b0fb9fd0380c6e56a46fa9902cb61946c2
gibgab67 <60315373+gibgab67@users.noreply.github.com>
2022-09-19 16:59:37 +0200

    Joueur V1

--- Application/Joueur/Joueur.java
+++ Application/Joueur/Joueur.java
@@ -1,6 +1,11 @@
 package Joueur;
 public class Joueur {
-
-    
+    int endurance;
+	Case case;
+	
+	public Joueur(int endurance, Case case) {
+		this.endurance = endurance;
+		this.case = case;
+	}
     
 }
